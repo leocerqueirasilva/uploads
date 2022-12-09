5140bdb74193131aed05cc51f432deb04bef1672
leocerqueirasilva <120220664+leocerqueirasilva@users.noreply.github.com>
2022-12-09 17:41:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
+        		var adobeDCView = new AdobeDC.View({clientId: "f23068fab94747d2867aa6162537f90b", divId: "adobe-dc-view"});
+        		adobeDCView.previewFile({
+        			content:{location: {url: "<?php echo $attachment->url; ?>"}},
+        			metaData:{fileName: "<?php echo $attachment->name; ?>"}
+        		}, {embedMode: "SIZED_CONTAINER"});
+        	});
+        	
+        	
+        	
+        	adobe();
